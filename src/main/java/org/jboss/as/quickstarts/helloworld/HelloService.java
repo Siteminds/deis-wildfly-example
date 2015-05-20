28905546a68ce2700013eae846f891eced706415
Bastiaan Schaap <b.schaap@siteminds.nl>
2015-05-20 22:01:06 +0200

    Change service to force new build

--- src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
+++ src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
@@ -9,7 +9,7 @@ package org.jboss.as.quickstarts.helloworld;
 public class HelloService {
 
     String createHelloMessage(String name) {
-        return "Hello " + name + "!";
+        return "'ello 'ello " + name + "!";
     }
 
 }
